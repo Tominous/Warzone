325a62235988af47498772dfa7edd734e1a68110
didjee2 <dieter08@live.be>
2017-07-17 18:28:36 +0200

    tes

--- src/com/cubedcraft/warzone/AdminCommands.java
+++ src/com/cubedcraft/warzone/AdminCommands.java
@@ -4,11 +4,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
 import net.md_5.bungee.api.ChatColor;
 
-
 public class AdminCommands implements CommandExecutor {
+	
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             if (!sender.hasPermission("warzone.admin")) {
